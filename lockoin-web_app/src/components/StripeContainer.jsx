e4ec1a15548c8f43c4409af46b3af18c4f1964b2
Monicah Koina <monicahkoina1@gmail.com>
2025-01-16 12:01:30 +0300

    code update

---
+++ lockoin-web_app/src/components/StripeContainer.jsx
@@ -0,0 +1,10 @@
+import 'react'
+
+const PUBLIC_KEY =
+    "pk_test_51QhW9KP3J6awdgv9LOcfGoBiJjionazvgPj8Zz9iSM0MVkWF3yOVDQn7vOQ95VcfmWTp0zO6dyqWIXjgTJJKgpeH00YGyuGQIJ";
+    const stripeTestLoad = 
+export default function StripeContainer() {
+  return (
+    <div>StripeContainer</div>
+  )
+}
