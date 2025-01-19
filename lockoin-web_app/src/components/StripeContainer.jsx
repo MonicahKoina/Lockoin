3c6f4245979db28bc0822031fb39335ebfafd613
Monicah Koina <monicahkoina1@gmail.com>
2025-01-19 22:15:04 +0300

    set up tailwind, add publishable key, create the transaction form for the buyer

--- lockoin-web_app/src/components/StripeContainer.jsx
+++ lockoin-web_app/src/components/StripeContainer.jsx
@@ -1,10 +1,15 @@
-import 'react'
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import "react";
+import PaymentForm from "./PaymentForm";
 
 const PUBLIC_KEY =
-    "pk_test_51QhW9KP3J6awdgv9LOcfGoBiJjionazvgPj8Zz9iSM0MVkWF3yOVDQn7vOQ95VcfmWTp0zO6dyqWIXjgTJJKgpeH00YGyuGQIJ";
-    const stripeTestLoad = 
+  "pk_test_51QhW9KP3J6awdgv9LOcfGoBiJjionazvgPj8Zz9iSM0MVkWF3yOVDQn7vOQ95VcfmWTp0zO6dyqWIXjgTJJKgpeH00YGyuGQIJ";
+const stripeTestPromise = loadStripe(PUBLIC_KEY);
 export default function StripeContainer() {
   return (
-    <div>StripeContainer</div>
-  )
+    <Elements stripe={stripeTestPromise}>
+      <PaymentForm />
+    </Elements>
+  );
 }
